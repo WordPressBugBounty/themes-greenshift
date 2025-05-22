823dd055a2ecbcd6d3f1f3a8694d90263f251ed0
Sean Murphy <sean@iamseanmurphy.com>
2025-05-22 16:19:45 +0000

    2.6.4

--- greenshift/functions.php
+++ greenshift/functions.php
@@ -9,7 +9,7 @@
  */
 
 if ( !defined( 'GREENSHIFT_THEME_VERSION' ) ) {
-	define('GREENSHIFT_THEME_VERSION', '2.6.3');
+	define('GREENSHIFT_THEME_VERSION', '2.6.4');
 }
 if ( !defined( 'GREENSHIFT_THEME_DIR' ) ) {
 	define('GREENSHIFT_THEME_DIR', get_template_directory_uri());
